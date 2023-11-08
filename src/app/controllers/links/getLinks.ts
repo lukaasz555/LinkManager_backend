a803ac0fd61c7896e86f191f7a1d1a6cbfd56aed
lukaasz555 <lukasz.zegar555@gmail.com>
2023-11-08 11:15:04 +0100

    quick getLinks update

--- src/app/controllers/links/getLinks.ts
+++ src/app/controllers/links/getLinks.ts
@@ -4,11 +4,21 @@ import { Link } from "../../models/Link";
 
 const getLinks = (req: Request, res: Response) => {
   try {
-    console.log("getLinks controller try");
-    res.send(mockLinks);
+    let filteredLinks = mockLinks;
+
+    if (req.query.categoryId) {
+      filteredLinks = mockLinks.filter((link) =>
+        link.categoriesIds.includes(Number(req.query.categoryId))
+      );
+    }
+    if (req.query.favorites) {
+      console.log("favorites: ", Boolean(req.query.favorites));
+      filteredLinks = filteredLinks.filter((link) => link.isFavorite);
+    }
+
+    return res.status(200).json(filteredLinks);
   } catch (err) {
-    console.log("getLinks controller catch");
-    res.status(500).json({ errorMessage: "/getLinks controller error" });
+    return res.status(500).json({ errorMessage: "GetLinks controller error" });
   }
 };
 
