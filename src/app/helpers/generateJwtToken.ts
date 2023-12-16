0bf3f5b5029e12bd43e8b270b9a2b14b2151b94f
lukaasz555 <97296160+lukaasz555@users.noreply.github.com>
2023-12-16 14:45:17 +0100

    temporary removed refresh token

--- src/app/helpers/generateJwtToken.ts
+++ src/app/helpers/generateJwtToken.ts
@@ -1,15 +1,18 @@
-import jwt from "jsonwebtoken";
+import jwt from 'jsonwebtoken';
 
 export function generateJwtToken(userId: string, email: string): string {
-  const tokenSecret = process.env.TOKEN_SECRET || "";
-  const expiresIn = "15m";
+	if (!process.env.TOKEN_SECRET) {
+		throw Error('There is TOKEN_SECRET declared in env');
+	}
+	const tokenSecret = process.env.TOKEN_SECRET;
+	const expiresIn = '15m';
 
-  const payload = {
-    sub: userId,
-    email,
-    iat: Math.floor(Date.now() / 1000),
-  };
+	const payload = {
+		sub: userId,
+		email,
+		iat: Math.floor(Date.now() / 1000),
+	};
 
-  const jwtToken = jwt.sign(payload, tokenSecret, { expiresIn });
-  return jwtToken;
+	const jwtToken = jwt.sign(payload, tokenSecret, { expiresIn });
+	return jwtToken;
 }
