97c2b2c30ca85ff64cc5285fe26bda3b66a19b2b
lukaasz555 <97296160+lukaasz555@users.noreply.github.com>
2023-12-17 16:40:52 +0100

    temporary added authMiddleware for logs

---
+++ src/app/middlewares/authMiddleware.ts
@@ -0,0 +1,9 @@
+import { Request, Response, NextFunction } from 'express';
+
+export function authMiddleware(
+	req: Request,
+	res: Response,
+	next: NextFunction
+) {
+	console.log(`authMiddleware: ${req.method} URL: ${req.url}`);
+}
--- src/app/routes/auth.ts
+++ src/app/routes/auth.ts
@@ -1,10 +1,12 @@
-import express from "express";
-import loginUser from "../controllers/auth/loginUser";
-import registerUser from "../controllers/auth/registerUser";
+import express from 'express';
+import loginUser from '../controllers/auth/loginUser';
+import registerUser from '../controllers/auth/registerUser';
+import { authMiddleware } from '../middlewares/authMiddleware';
 
 const router = express.Router();
 
-router.route("/login").post(loginUser);
-router.route("/register").post(registerUser);
+router.use(authMiddleware);
+router.route('/login').post(loginUser);
+router.route('/register').post(registerUser);
 
 export default router;
